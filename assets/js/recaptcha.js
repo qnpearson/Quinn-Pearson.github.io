391f8a89e8d1f83d24b2780c9f434a18156c8a20
Quinn Pearson <135060769+qnpearson@users.noreply.github.com>
2024-09-28 20:36:52 -0400

    Update recaptcha.js

--- assets/js/recaptcha.js
+++ assets/js/recaptcha.js
@@ -1,27 +1,30 @@
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-  e.preventDefault(); // Prevent default form submission
+<!-- recaptcha.js -->
+<script>
+  document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent default form submission
 
-  // Execute reCAPTCHA
-  grecaptcha.enterprise.execute('6LfNLlEqAAAAADJeZlc_8Y64x1SvTdyQi3UnDh4B', {action: 'submit'})
-    .then(function (token) {
-      if (token) {
-        // Add the reCAPTCHA token as a hidden field in the form
-        const recaptchaResponseInput = document.createElement('input');
-        recaptchaResponseInput.setAttribute('type', 'hidden');
-        recaptchaResponseInput.setAttribute('name', 'g-recaptcha-response');
-        recaptchaResponseInput.setAttribute('value', token);
+    // Execute reCAPTCHA
+    grecaptcha.enterprise.execute('6LfNLlEqAAAAADJeZlc_8Y64x1SvTdyQi3UnDh4B', {action: 'submit'})
+      .then(function (token) {
+        if (token) {
+          // Add the reCAPTCHA token as a hidden field in the form
+          const recaptchaResponseInput = document.createElement('input');
+          recaptchaResponseInput.setAttribute('type', 'hidden');
+          recaptchaResponseInput.setAttribute('name', 'g-recaptcha-response');
+          recaptchaResponseInput.setAttribute('value', token);
 
-        // Append the hidden field to the form
-        document.getElementById('contact-form').appendChild(recaptchaResponseInput);
+          // Append the hidden field to the form
+          document.getElementById('contact-form').appendChild(recaptchaResponseInput);
 
-        // Now submit the form to Formspree
-        document.getElementById('contact-form').submit();
-      } else {
-        alert('reCAPTCHA verification failed. Please try again.');
-      }
-    })
-    .catch(function (error) {
-      console.error('reCAPTCHA error:', error);
-      alert('Error verifying reCAPTCHA. Please try again.');
-    });
-});
+          // Now submit the form
+          document.getElementById('contact-form').submit();
+        } else {
+          alert('reCAPTCHA verification failed. Please try again.');
+        }
+      })
+      .catch(function (error) {
+        console.error('reCAPTCHA error:', error);
+        alert('Error verifying reCAPTCHA. Please try again.');
+      });
+  });
+</script>
